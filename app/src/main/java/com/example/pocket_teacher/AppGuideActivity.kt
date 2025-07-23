9a3094007e810a40986a1a3792f4eefaa077ee8e
mjk0323 <jk008532@gmail.com>
2025-07-23 13:34:14 +0900

    ✨인앱에서 카카오톡, 유튜브 앱 열기 기능 추가

--- app/src/main/java/com/example/pocket_teacher/AppGuideActivity.kt
+++ app/src/main/java/com/example/pocket_teacher/AppGuideActivity.kt
@@ -9,6 +9,20 @@ class AppGuideActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_app_guide)
 
+        val kakaoPackage = "com.kakao.talk" // 카톡 앱 패키지 주소
+        val intentKakao = packageManager.getLaunchIntentForPackage(kakaoPackage)
+        val kakaoButton = findViewById<LinearLayout>(R.id.button_kakao)
+        kakaoButton.setOnClickListener {
+            startActivity(intentKakao)
+        }
+
+        val youtubePackage = "com.google.android.youtube" // 유튜브 앱 패키지 주소
+        val intentYoutube = packageManager.getLaunchIntentForPackage(youtubePackage)
+        val youtubeButton = findViewById<LinearLayout>(R.id.button_youtube)
+        youtubeButton.setOnClickListener {
+            startActivity(intentYoutube)
+        }
+
         val backButton = findViewById<LinearLayout>(R.id.button_back)
         backButton.setOnClickListener {
             finish() // 이전 화면으로 돌아감
