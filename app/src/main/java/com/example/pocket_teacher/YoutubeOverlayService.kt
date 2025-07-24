4f754fb6b8a20eade5e0047eb43fbf1dd24b7cd3
mjk0323 <jk008532@gmail.com>
2025-07-24 17:13:24 +0900

    ✨앱 실행 시 오버레이 표시

---
+++ app/src/main/java/com/example/pocket_teacher/YoutubeOverlayService.kt
@@ -0,0 +1,61 @@
+package com.example.pocket_teacher
+
+import android.app.Service
+import android.content.Intent
+import android.graphics.PixelFormat
+import android.os.Build
+import android.os.IBinder
+import android.view.Gravity
+import android.view.LayoutInflater
+import android.view.View
+import android.view.WindowManager
+import androidx.annotation.RequiresApi
+
+class YoutubeOverlayService : Service() {
+    private lateinit var windowManager: WindowManager
+    private var overlayView: View? = null
+
+    override fun onBind(intent: Intent?): IBinder? = null
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        val targetPackage = intent?.getStringExtra("target_package")
+
+        if (targetPackage == "com.google.android.youtube") {
+            showOverlay()
+        }
+
+        return START_STICKY
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun showOverlay() {
+        windowManager = getSystemService(WINDOW_SERVICE) as WindowManager
+
+        // 오버레이 뷰 생성
+        overlayView = LayoutInflater.from(this).inflate(R.layout.service_youtube_overlay, null)
+
+        // 윈도우 매니저 파라미터 설정
+        val params = WindowManager.LayoutParams(
+            WindowManager.LayoutParams.WRAP_CONTENT,
+            WindowManager.LayoutParams.WRAP_CONTENT,
+            WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY, // Android 8.0+
+            WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
+            PixelFormat.TRANSLUCENT
+        )
+
+        params.gravity = Gravity.TOP or Gravity.START
+        params.x = 100
+        params.y = 100
+
+        // 오버레이 추가
+        windowManager.addView(overlayView, params)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        if (overlayView != null) {
+            windowManager.removeView(overlayView)
+        }
+    }
+}
